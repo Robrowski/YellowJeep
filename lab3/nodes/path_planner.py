3720fb78dc661aa3b6f14bc522dc89afa334ba2e
Robrowski <rpdabrowski@wpi.edu>
2014-04-04 16:53:38 -0400

    functions that take x,y coordinates now take Point objects @TimMurcko

--- lab3/nodes/path_planner.py
+++ lab3/nodes/path_planner.py
@@ -11,14 +11,23 @@ from geometry_msgs.msg import Point
 
 
 
+#####################################################
+##########   Grid Cell Utilities   ##################
+#####################################################
 # makes a grid cell with the x/y indices from the map
-def newGridCell(x, y):
-    global gridOrigin
-    global gridResolution
-    
-    gx = gridOrigin.x + x*gridResolution
-    gy = gridOrigin.y + y*gridResolution
-    return Point(gx, gy, 0)
+# can also take a Point object
+def newGridCell(x, y = None):
+	global gridOrigin
+	global gridResolution
+	
+	#X is actually a Point object in this case
+	if y == None: 
+		y = x.y
+		x = x.x
+
+	gx = gridOrigin.x + x*gridResolution	
+	gy = gridOrigin.y + y*gridResolution
+	return Point(gx, gy, 0)
 
 
 # Takes cells as a list of Points and sends them to rviz
@@ -27,11 +36,16 @@ def makeGridCells(cells):
 	newHeader = Header(1,rospy.get_rostime(),'map')
 	return GridCells(newHeader, gridResolution, gridResolution, cells)
 
-
+#####################################################
+################   Map Utilities   ##################
+#####################################################
 # Reads map meta data and saves to globals
 def readMapMetaData(aMap):
 	global mapOrigin
 	mapOrigin  = aMap.info.origin.position 
+
+	global mapInfo
+	mapInfo = aMap.info
 
 	global gridResolution  
 	gridResolution = aMap.info.resolution # assume square map
@@ -42,6 +56,21 @@ def readMapMetaData(aMap):
 	gridOrigin = Point( mapOrigin.x + gridResolution/2  , mapOrigin.y + gridResolution/2  ,  0)
 
 
+# returns the value at the given point
+# can also take a Point object
+def readMapPoint(x,  y = None):
+	global currentMap
+	
+	#X is actually a Point object in this case
+	if y == None: 
+		y = x.y
+		x = x.x
+	
+	width = currentMap.info.width	
+	return currentMap.data[y*width + x]
+
+#####################################################
+###################   PUBLISHERS   ##################
 #####################################################
 # Send cells to the frontier topic
 def sendToFrontier(cells):
@@ -69,35 +98,57 @@ def mapRecieved(newMap):
 
 	readMapMetaData(newMap);
 
+	
+	######################################################
+	# OPTIONAL ARGUMENTS RULE
+	print "Point 1"
+	print newGridCell(           7, 5)
+ 	
+ 	print "Point 2"
+ 	print newGridCell(Point(7,5,0)   )
+ 
+	# Read map Point
+	print "read 1"
+	print readMapPoint(0,0)
+	
+	print "read 2"
+	print readMapPoint(   Point(0,0,0) )
+	######################################################
 
+	#mapAnimationDemo();
 
-
-
-	a = []
-	b = []
-	c = []
-	for x in range(0, 37, 3):
-		for y in range(0,37, 1):
-			a += [newGridCell(x,y)]
-			b += [newGridCell(x+1,y+2)]
-			c += [newGridCell(x+2,y+2)]
-
-	sendToPath(a)
-	sendToFrontier( b )
-	sendToExpanded(c)
-	print "poop bitchz"
-
-
+	
 # initializes frontier, expanded and path publishers
 def initGridCellPublishers():
- 	global frontPub
-    frontPub = rospy.Publisher('/frontier', GridCells);
-    
-    global expandedPub
-    expandedPub = rospy.Publisher('/expandedPub', GridCells);
-    
-    global pathPub
-    pathPub = rospy.Publisher('/pathPub', GridCells);
+	global frontPub
+	frontPub = rospy.Publisher('/frontier', GridCells)
+
+	global expandedPub
+	expandedPub = rospy.Publisher('/expandedPub', GridCells)
+
+	global pathPub
+	pathPub = rospy.Publisher('/pathPub', GridCells)
+
+
+
+#demo for map reading and animation
+def mapAnimationDemo():
+	#### EXAMPLE FOR COLORING THE MAP 
+	global mapInfo
+	front = []
+	exp = []
+	r = rospy.Rate(50)# 50hz
+	for x in range(mapInfo.width):
+		for y in range(mapInfo.height):
+			ptVal = readMapPoint(x,y)
+			if ptVal == 0:
+				front += [newGridCell(x,y)]
+			else:
+				exp   += [newGridCell(x,y)]
+			sendToExpanded(exp)
+			sendToFrontier(front)
+			r.sleep()
+
 
 
 
@@ -106,13 +157,14 @@ def initGridCellPublishers():
 if __name__ == '__main__':
     # Change this node name to include your username
     rospy.init_node('Path_Planner', anonymous=True)
-
-	initGridCellPublishers()
+    
+    initGridCellPublishers()
    
   	# Subscribing to the map
     rospy.Subscriber('/map',  OccupancyGrid, mapRecieved, queue_size=1)
-
-
+    
+  
+    
     # Wait, then spin. 
     rospy.sleep(rospy.Duration(.5, 0))       
     rospy.spin()
