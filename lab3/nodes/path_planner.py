b0c7f9bb431515e73b660c5aec693e2d4793d09a
Robrowski <rpdabrowski@wpi.edu>
2014-04-04 15:06:15 -0400

    further abstraction of gridcells and occupancy grid  @TimMurcko

--- lab3/nodes/path_planner.py
+++ lab3/nodes/path_planner.py
@@ -13,31 +13,51 @@ from geometry_msgs.msg import Point
 # handy!
 # roslaunch turtlebot_rviz_launchers view_navigation.launch
 
+
+def newGridCell(x, y):
+    global gridOrigin
+    global gridResolution
+    
+    gx = gridOrigin.x + x*gridResolution
+    gy = gridOrigin.y + y*gridResolution
+    return Point(gx, gy, 0)
+
+
 # Prints back map data
 def printMap(newMap):
-    global frontPub
-    global currentMap
-    global mapOrigin
-    global gridResolution
-    currentMap = newMap
-    mapOrigin  = newmap.info.origin.position   
-    gridResolution = newMap.info.resolution/2 # assume square map
-    
-    # Our grid has double the resolution of the map, so the /2
-    # centers grid squares on the map
-    gridOrigin = Point( mapOrigin.x + gridResolution/2  , mapOrigin.y + gridResolution/2  ,  0);
-   
-    
-    x = mapOrigin.x + gridResolution/2
-    y = mapOrigin.y + gridResolution/2
-    
-
-    newGrid = GridCells(Header(1,rospy.get_rostime(),'map'), gridResolution, gridResolution, [Point(x,y,0)])
-
-    frontPub.publish(newGrid)
-    print "poop bitchz"
+	global frontPub
+	global currentMap
+	global mapOrigin
 
 
+	currentMap = newMap
+	readMapMetaData(newMap);
+
+	newGrid = makeGridCells([newGridCell( 15,30 ),newGridCell( 30 ,30 ),newGridCell( 27 , 20 ), newGridCell(0,0)])
+	frontPub.publish(newGrid)
+	print "poop bitchz"
+
+
+# Takes cells as a list of Points and sends them to rviz
+def makeGridCells(cells):
+	global gridResolution
+	newHeader = Header(1,rospy.get_rostime(),'map')
+	return GridCells(newHeader, gridResolution, gridResolution, cells)
+	
+
+
+	# Reads map meta data and saves to globals
+def readMapMetaData(aMap):
+	global mapOrigin
+	mapOrigin  = aMap.info.origin.position 
+
+	global gridResolution  
+	gridResolution = aMap.info.resolution # assume square map
+
+	global gridOrigin
+	# Our grid has double the resolution of the map, so the /2
+	# centers grid squares on the map
+	gridOrigin = Point( mapOrigin.x + gridResolution/2  , mapOrigin.y + gridResolution/2  ,  0)
 
 
 
