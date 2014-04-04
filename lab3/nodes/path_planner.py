3bef3932b37c3c5a13e0957c345418ff70917c64
Robrowski <rpdabrowski@wpi.edu>
2014-04-04 15:46:07 -0400

    can publish multiple grid cell colors @Tim Murcko

--- lab3/nodes/path_planner.py
+++ lab3/nodes/path_planner.py
@@ -10,10 +10,8 @@ from std_msgs.msg import Header
 from geometry_msgs.msg import Point
 
 
-# handy!
-# roslaunch turtlebot_rviz_launchers view_navigation.launch
 
-
+# makes a grid cell with the x/y indices from the map
 def newGridCell(x, y):
     global gridOrigin
     global gridResolution
@@ -23,30 +21,14 @@ def newGridCell(x, y):
     return Point(gx, gy, 0)
 
 
-# Prints back map data
-def printMap(newMap):
-	global frontPub
-	global currentMap
-	global mapOrigin
-
-
-	currentMap = newMap
-	readMapMetaData(newMap);
-
-	newGrid = makeGridCells([newGridCell( 15,30 ),newGridCell( 30 ,30 ),newGridCell( 27 , 20 ), newGridCell(0,0)])
-	frontPub.publish(newGrid)
-	print "poop bitchz"
-
-
 # Takes cells as a list of Points and sends them to rviz
 def makeGridCells(cells):
 	global gridResolution
 	newHeader = Header(1,rospy.get_rostime(),'map')
 	return GridCells(newHeader, gridResolution, gridResolution, cells)
-	
 
 
-	# Reads map meta data and saves to globals
+# Reads map meta data and saves to globals
 def readMapMetaData(aMap):
 	global mapOrigin
 	mapOrigin  = aMap.info.origin.position 
@@ -60,21 +42,75 @@ def readMapMetaData(aMap):
 	gridOrigin = Point( mapOrigin.x + gridResolution/2  , mapOrigin.y + gridResolution/2  ,  0)
 
 
+#####################################################
+# Send cells to the frontier topic
+def sendToFrontier(cells):
+	global frontPub
+	frontPub.publish(makeGridCells(cells))
+	
+
+# Send cells to the path topic
+def sendToPath(cells):
+	global pathPub
+	pathPub.publish(makeGridCells(cells))
+
+# Send expanded to the path topic
+def sendToExpanded(cells):
+	global expandedPub
+	expandedPub.publish(makeGridCells(cells))
+
+
+	
+######################################################
+# Prints back map data
+def mapRecieved(newMap):
+	global currentMap
+	currentMap = newMap
+
+	readMapMetaData(newMap);
+
+
+
+
+
+	a = []
+	b = []
+	c = []
+	for x in range(0, 37, 3):
+		for y in range(0,37, 1):
+			a += [newGridCell(x,y)]
+			b += [newGridCell(x+1,y+2)]
+			c += [newGridCell(x+2,y+2)]
+
+	sendToPath(a)
+	sendToFrontier( b )
+	sendToExpanded(c)
+	print "poop bitchz"
+
+
+# initializes frontier, expanded and path publishers
+def initGridCellPublishers():
+ 	global frontPub
+    frontPub = rospy.Publisher('/frontier', GridCells);
+    
+    global expandedPub
+    expandedPub = rospy.Publisher('/expandedPub', GridCells);
+    
+    global pathPub
+    pathPub = rospy.Publisher('/pathPub', GridCells);
+
+
 
 
 # This is the program's main function
 if __name__ == '__main__':
-
     # Change this node name to include your username
     rospy.init_node('Path_Planner', anonymous=True)
 
-
-    # Subscribing to the map
-    rospy.Subscriber('/map',  OccupancyGrid, printMap, queue_size=1)
-
-
-    global frontPub
-    frontPub = rospy.Publisher('/frontier', GridCells);
+	initGridCellPublishers()
+   
+  	# Subscribing to the map
+    rospy.Subscriber('/map',  OccupancyGrid, mapRecieved, queue_size=1)
 
 
     # Wait, then spin. 
