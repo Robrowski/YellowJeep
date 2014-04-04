831adeb1b0963ae5466aed86fcbff97659638181
Robrowski <rpdabrowski@wpi.edu>
2014-04-03 20:28:31 -0700

    completed prelab for lab3... i guess..

---
+++ robertpdabrowski_lab2/scripts/lab3_prelab.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+##############################################################################
+# Ros Imports
+##############################################################################
+import rospy, math
+from nav_msgs.msg import OccupancyGrid
+
+
+
+# handy!
+# roslaunch turtlebot_rviz_launchers view_navigation.launch
+
+# Prints back map data
+def printMap(data):
+	print data
+
+
+
+
+
+# This is the program's main function
+if __name__ == '__main__':
+    
+	# Change this node name to include your username
+	rospy.init_node('Rob made a map reading Node', anonymous=True)
+
+
+	# Subscribing to the map
+	rospy.Subscriber('/map',  OccupancyGrid, printMap, queue_size=1)
+
+	# Wait, then spin. 
+	rospy.sleep(rospy.Duration(.5, 0))       
+	rospy.spin()
+
