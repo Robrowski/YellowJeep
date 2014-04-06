c684a739723a36bae45c6ca39742e3e804f69134
mscosti <mscosti@wpi.edu>
2014-04-06 18:17:23 -0400

    added comments to a star algorithm and helpers, and properly implemented g_score

--- lab3/nodes/path_planner.py
+++ lab3/nodes/path_planner.py
@@ -21,8 +21,14 @@ def mapRecieved(newMap):
 
 	#sendToPath( [Point(36,36,0)])
 	# mapAnimationDemo();
-	astar(Point(3,3,0),Point(10,4,0))
+	astar(Point(3,3,0),Point(11,15,0))
 
+######################################################
+# A-Star Helper Functions
+######################################################
+
+# given the current frontier and a dictionary of all 
+# f_scores, return the lowest scoring node on the frontier
 def getLowestF(frontier,f_score):
 	values = {}
 	for point in frontier:
@@ -30,9 +36,25 @@ def getLowestF(frontier,f_score):
 			values[point] = f_score[point]
 	return min(values,key=values.get)
 
-def weightBetween(x,y):
+# consider implementing a priority queue for the frontier, to
+# avoid going back and forth from frontier list to f_score
+# dictionary back to a point, like currently used in
+# getLowestF()
+def frontierPriorityPush():
+	pass
+
+# returns the weight/cost to go frome point a to point b
+# for simplicity, all weights are equal for the time being
+def weightBetween(a,b):
 	return 1
 
+# given the list of parents and the start and goal points,
+# return a reconstructed path going from the goal to the start
+# 
+# ****
+# The output of the path is a list of points to go to in order 
+# starting at the goal
+# ****
 def reconstructPath(parents,start,goal):
 	global holster
 	global pub
@@ -55,53 +77,57 @@ def reconstructPath(parents,start,goal):
 def astar(start, goal):
 	global holster
 	global pub
-	# global r
 
-	explored = []
-	frontier = [start]
+	explored = [] 		#list of explored nodes
+	frontier = [start]	#list of nodes to explore
 
-	g_score = {start:0}
-	f_score = {start:g_score[start] + distance(start, goal)}
-	parents = {}
+	g_score = {start:0} #dictionary for best cost to any node from start
+
+	#dictionary of best final scores ( = with heuristic) for each node from start
+	f_score = {start:g_score[start] + distance(start, goal)} 
+
+	parents = {} #dictionary representing node to parent relationship. ex: {node: parent}
 	
-	r = rospy.Rate(100)# 50hz
+	r = rospy.Rate(100)# 100hz for animation
+
 	while frontier:
+		#get the lowest node from the frontier to exlore next
 		current = getLowestF(frontier,f_score)
-		print "current"
 		print current.x, current.y
-		# print frontier
+
+		#if at the goal, add the goal to the parents list and end
 		if current == goal:
 			parents[goal] = current
 			break
 
+		#add the node to the explored list, and remove it from frontier
 		explored.append(current)
 		frontier.remove(current)
 
+		#for animating, publish on every iteration
 		pub.sendToExpanded(holster,explored)
 		pub.sendToFrontier(holster,frontier)
+
+		#get all eight connected neighbors
 		neighbors = holster.getEightAdjacentPoints(current)
 		for neighbor in neighbors:
 
-			if neighbor not in g_score:
-				g_score[neighbor] = weightBetween(current,neighbor)
-				temp_g = g_score[neighbor]
-			else:
-				temp_g = g_score[neighbor] + weightBetween(current,neighbor)
-			temp_f =  distance(neighbor,goal)
+			temp_g = g_score[current] + weightBetween(current,neighbor)
+			temp_f =  temp_g + distance(neighbor,goal)
+
+			print neighbor.x, neighbor.y, temp_g
 
 			if neighbor not in f_score:
 				f_score[neighbor] = temp_f
-			#if neighbor is already on the frontier and not lower than the value
-			# already in f_score, continue on to the next neighbor
-			
-			
 
+			#if neighbor is already explored and not lower than the value
+			# already in f_score, continue on to the next neighbor
 			if (neighbor in explored) and temp_f >= f_score[neighbor]:
-				print "already on frontier and is bad", neighbor.x, neighbor.y
+				# print "already in explored and is bad", neighbor.x, neighbor.y
 				continue
 
 			if (neighbor not in frontier) or temp_f < f_score[neighbor]:
-				print "not in frontier"
+				# print "not in frontier"
 				parents[neighbor] = current
 				g_score[neighbor] = temp_g
 				f_score[neighbor] = temp_f
