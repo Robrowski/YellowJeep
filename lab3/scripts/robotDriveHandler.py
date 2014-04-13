9f15ac41865911e6e026df1187c61f8fb4d4d33b
mscosti <mscosti@wpi.edu>
2014-04-13 12:26:06 -0400

    Robot Drive Handler should use waypoints published by astar

--- lab3/scripts/robotDriveHandler.py
+++ lab3/scripts/robotDriveHandler.py
@@ -2,7 +2,7 @@
 import rospy, math
 
 from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, GridCells
 from geometry_msgs.msg import Point
 from YellowPublisher import *
 from MapHolster import *
@@ -24,20 +24,58 @@ def read_odometry(data):
 	#print current_theta
 
 
-def followPath(waypoints):
-	global current_theta
+def followPath():
+	global current_theta, waypoints
 	fast = .4
 	waypoints.reverse()
-	for i in range(len(waypoints) - 1):	
-		current = waypoints[i]
-		next = waypoints[i+1]
+	# for i in range(len(waypoints) - 1):	
+	while True and not rospy.is_shutdown():
+		if len(waypoints) <= 1:
+			continue
+		current = waypoints[0]
+		next = waypoints[1]
 		print current
 		print next			
 		positionVector = unitVector(current,next)
 		desiredTheta = math.atan2(positionVector.x, positionVector.y)
 		rotate(desiredTheta - current_theta)
 		driveStraight(fast,distance(current,next))
+		waypoints.pop(0)
 		
+def gotWaypoints(waypointMsg):
+	global wayPoints
+	wayPoints = waypointMsg.cells
+
+
+
+if __name__ == '__main__':
+    
+    # Change this node name to include your username\
+	rospy.init_node('pathFollow', anonymous=True)
+	global current_theta
+	global robotOrientation
+	global pub
+	global wayPoints
+	wayPoints = [Point(0,0,0),Point(0,0,0)]
+	pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist)
+	
+	rospy.Subscriber('odom', Odometry, read_odometry, queue_size=1) 
+	rospy.Subscriber('/waypoints', GridCells, gotWaypoints
+		)
+	# Wait, then spin. Exectute trajectory activated by bumper events
+	rospy.sleep(rospy.Duration(.5, 0)) 
+	# wayPoints = [Point(4,2,0), Point(5,2,0), Point(5,3,0), Point(4,3,0),  Point(4,2,0)]
+	followPath(wayPoints)
+	      
+	rospy.spin()
+
+
+
+#######################################################################
+#
+#		Drive Helper Functions
+#
+#######################################################################
 
 # Helper function to make a Twist object with the given vlaues
 def makeTwist( forwardVelocity, angularVelocity):
@@ -52,44 +90,6 @@ def makeTwist( forwardVelocity, angularVelocity):
     twist.angular.z = angularVelocity;
   
     return twist
-
-#Takes a speed and a distance for the robot to move in a straight line
-def driveStraight(speed, distance):
-    global current_pose
-    start_pose = current_pose
-    
-    r = rospy.Rate(10) # 10hz
-    timePassed = 0.0
-    
-    while not rospy.is_shutdown() and math.fabs(calcDistance(start_pose, current_pose) - distance) > .05:
-       sendTwist(math.copysign(speed, distance), 0)
-       timePassed += 0.1;
-       r.sleep()
-    sendTwist(0,0)#stop 
-
-	
-#Accepts an angle and makes the robot rotate around it.
-'''
-def rotate(angle):
-    global current_theta
-   
-    goal_theta =  current_theta + angle
-
-   # make sure goal theta is within bounds of +/- pi
-    if goal_theta > math.pi:
-        goal_theta -= 2*math.pi
-        
-    if goal_theta < -math.pi:    
-        goal_theta += 2*math.pi
-    
-    # Rotate in optimal direction until at setpoint
-    r = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown() and  math.fabs(current_theta - goal_theta )  > math.pi/100  : 
-       sendTwist(0, math.copysign(.8, angle))
-       r.sleep()
-    
-    sendTwist(0,0)#stop
-'''
 
 def angleDelta(prev,current):
 	return abs(prev - current)
@@ -129,6 +129,20 @@ def rotateDegs(angle):
 		pub.publish(makeTwist(0,omega))
 		r.sleep()
 
+#Takes a speed and a distance for the robot to move in a straight line
+def driveStraight(speed, distance):
+    global current_pose
+    start_pose = current_pose
+    
+    r = rospy.Rate(10) # 10hz
+    timePassed = 0.0
+    
+    while not rospy.is_shutdown() and math.fabs(calcDistance(start_pose, current_pose) - distance) > .05:
+       sendTwist(math.copysign(speed, distance), 0)
+       timePassed += 0.1;
+       r.sleep()
+    sendTwist(0,0)#stop 
+
 def rotate(radians):
 	degs = (radians * 180) / math.pi
 	rotateDegs(degs)
@@ -141,24 +155,3 @@ def sendTwist(forwardVelocity, angularVelocity):
 # Calculates the magnitude of the distance between two points in the x-y plane
 def calcDistance(start, dst):
     return math.sqrt( math.pow(dst.position.x - start.position.x,2) + math.pow(dst.position.y - start.position.y,2))
-
-if __name__ == '__main__':
-    
-    # Change this node name to include your username\
-	rospy.init_node('pathFollow', anonymous=True)
-	global current_theta
-	global robotOrientation
-	global pub
-	pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist)
-	
-	rospy.Subscriber('odom', Odometry, read_odometry, queue_size=1) 
-	# Wait, then spin. Exectute trajectory activated by bumper events
-	rospy.sleep(rospy.Duration(.5, 0)) 
-	wayPoints = [Point(4,2,0), Point(5,2,0), Point(5,3,0), Point(4,3,0),  Point(4,2,0)]
-	followPath(wayPoints)
-	      
-	rospy.spin()
-
-
-
-
